c53ffd08c5a5034af36ef761e7a4bfda02223c4b
Mikhail Renskiy <71692804+EmperorsChosen@users.noreply.github.com>
2024-11-26 00:35:17 +0200

    Update user_service.py

--- user_service.py
+++ user_service.py
@@ -32,4 +32,4 @@ def login():
     return jsonify({"message": "Login successful"}), 200
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
